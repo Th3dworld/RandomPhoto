27f9a55e159e5a7516752561a86a85eff338664c
Matanda Phiri <matandaphiri64@gmail.com>
2025-01-09 15:21:44 -0600

    Initial Commit

---
+++ RandomPhotoTests/RandomPhotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoTests.swift
+//  RandomPhotoTests
+//
+//  Created by Matanda  Phiri  on 1/9/25.
+//
+
+import Testing
+@testable import RandomPhoto
+
+struct RandomPhotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
